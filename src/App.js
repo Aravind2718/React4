9b458682365d33a6c90711b10204e9f346fe92dc
Aravind2718 <108514269+Aravind2718@users.noreply.github.com>
2023-03-31 14:26:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './comp/form';
+function App() {
+  return(
+    <div>
+      <Form/>
+
+    </div>
+  );
+}
+export default App;
+
+
+
+
+
+
+
+
